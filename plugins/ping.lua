c4654db69ffb8bbc8e3715d3709dd1bb3cf8d1cc
gold bird <pouria346h@gmail.com>
2016-12-14 23:06:52 +0330

    Add files via upload

---
+++ plugins/ping.lua
@@ -0,0 +1,18 @@
+﻿do
+
+function run(msg, matches)
+send_document(get_receiver(msg), "./data/sticker.gif", ok_cb, false)
+if matches[1] == 'انلاینی' then
+    if not is_momod(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
+return {
+patterns = {
+    "انلاینی",
+},
+run = run
+}
+
+end
